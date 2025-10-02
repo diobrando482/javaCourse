36fd6bac214217126049605f3e9ca0dfe73cf942
diobrando482 <eboirazban@gmail.com>
2025-10-02 20:07:42 +0600

    'some-commits'

---
+++ src/beecrowd/Problem1002.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+
+public class Main{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        double r = s.nextDouble();
+        System.out.println("A=%.4f%n",3.14*r*r)
+    }
+}
